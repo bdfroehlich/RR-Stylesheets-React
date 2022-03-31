12dd50968f0e1a5f8796906f5a8d036d2622772e
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-03-30 21:30:11 -0500

    3 micro style sheets passed as props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import reactDom from 'react-dom';
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Profile from './components/Profile';
+
+const aboutStyle = {
+  fontSize: "25px",
+  textAlign: "center",
+  margin: "40px",
+  border: "1px solid green",
+  background: "orange",
+  color: "pink"
+};
+
+const contactStyle = {
+  fontSize: "3vw",
+  textAlign: "center",
+  margin: "20px",
+  border: "5px dashed red",
+  background: "blue",
+  color: "white"
+};
+
+const profileStyle = {
+  fontSize: "5vw",
+  textAlign: "center",
+  margin: "10px",
+  border: "20px solid blue",
+  background: "black",
+  color: "green"
+};
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <About aboutStyle={aboutStyle} />
+      <Contact contactStyle={contactStyle} />
+      <Profile profileStyle={profileStyle} />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About(props) {
+  return (
+    <div style={props.aboutStyle}>
+      <h1>About Us</h1>
+    </div>
+  );
+}
+
+export default About;
